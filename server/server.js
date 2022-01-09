502cd3d6aa78cdad4173476554cbba4e445ac024
Gratian Permien <gratian.permien@gmail.com>
2022-01-09 18:01:42 +0100

    client & server working on 3000 and 4000

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 import { dirname } from "./lib/pathHelpers.js";
 import path from "path";
 import express from "express";
+import cors from "cors";
 
 const __dirname = dirname(import.meta.url);
-const server = express();
 
+const server = express();
 const port = process.env.PORT || 4000;
+
+server.use(cors());
+server.use(express.json());
 
 server.get("/api", (req, res) => {
   res.json({ message: "Hi, express server is present" });
 });
 
-server.use(express.static(path.join(__dirname, "../client/dist")));
+server.use(express.static(path.join(__dirname, "../client/dist/")));
 
 server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/", "index.html"));
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
