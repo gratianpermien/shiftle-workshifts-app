c5dca8d22c761ae6322738ecf39e1f4df8c0ee98
Gratian Permien <gratian.permien@gmail.com>
2022-01-09 16:34:50 +0100

    pre Heroku fix5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,11 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [myHeadline, setMyHeadline] = useState("");
 
   useEffect(() => {
     const fetchMyHeadline = async () => {
-      const response = await fetch("http://localhst:4000/api");
+      const response = await fetch("http://localhost:4000/api");
       const data = await response.json();
       setMyHeadline(data.message);
     };
