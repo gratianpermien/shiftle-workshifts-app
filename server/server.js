8bd30c4fdd4dce995302b7ba52ece327bb7bfc5c
Gratian Permien <gratian.permien@gmail.com>
2022-01-09 15:55:25 +0100

    pre Heroku fix 4

--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ import { dirname } from "./lib/pathHelpers.js";
 import path from "path";
 import express from "express";
 
+const __dirname = dirname(import.meta.url);
 const server = express();
-server.use(express.static(path.join(__dirname, "../client/dist")));
-// server.use(cors());
 
 const port = process.env.PORT || 4000;
 
 server.get("/api", (req, res) => {
   res.json({ message: "Hi, express server is present" });
+});
+
+server.use(express.static(path.join(__dirname, "../client/dist")));
+
+server.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
