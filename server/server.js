be85ca05c16eb44f7ea093c4ab459fcabd425c29
Gratian Permien <gratian.permien@gmail.com>
2022-01-09 14:26:52 +0100

    node_modules be gone

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-import {dirname} from "./lib/pathHelpers.js"
-import express from "express"
-import cors from "cors"
-cost server = express()
-server.use(cors())
-const port = process.env.PORT || 4000
-server.get('/api', (req, res)=> {
-    res.json({message:'Hi, express server is present'})
-})
+import { dirname } from "./lib/pathHelpers.js";
+import path from "path";
+import express from "express";
+import cors from "cors";
 
-server.listen (port, () => console.log(`Server running on port ${port}`))
+const server = express();
+
+server.use(cors());
+
+const port = process.env.PORT || 4000;
+
+server.get("/api", (req, res) => {
+  res.json({ message: "Hi, express server is present" });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
