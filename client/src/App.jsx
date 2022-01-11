b108a4bfb344c9a748f159b2f517c86dc373e661
Gratian Permien <gratian.permien@gmail.com>
2022-01-11 13:08:40 +0100

    structured object data available

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,33 +4,67 @@ import "./App.css";
 function App() {
   const [bookingDataFromMonday, setBookingDataFromMonday] = useState("");
 
-  // const api_monday = import.meta.env.VITE_API_KEY_MONDAY;
-
+  //Prepare asynchronous fetch from Monday GraphQL API and store in "data"
   useEffect(() => {
     const fetchBookingDataFromMonday = async () => {
-      const query =
-        "{ boards (ids: 1096884858) { name state board_folder_id items {id name column_values {title text}}}}";
+      const query = `{ boards (ids: 1096884858) {items (limit: 20) {id column_values {title text}}}}`; //import.meta.env.VITE_BOOKINGS_BOARD_ID_MONDAY;
+      const monday_api =
+        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjEzOTgwOTU0NCwidWlkIjoxODQ1NzI4OCwiaWFkIjoiMjAyMi0wMS0wN1QxNzowMjo0OC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Nzk5ODY1NywicmduIjoidXNlMSJ9.gSFPbRKfcDcuEa-r355sBAVAQS_vDhwNvTP3IIFOhL4"; //import.meta.env.VITE_API_KEY_MONDAY;
       const response = await fetch("https://api.monday.com/v2", {
         method: "post",
         headers: {
           "Content-Type": "application/json",
-          Authorization: import.meta.env.VITE_API_KEY_MONDAY,
+          Authorization: monday_api,
         },
         body: JSON.stringify({
           query: query,
         }),
       });
       const data = await response.json();
-      console.log(JSON.stringify(data, null, 2));
-      setBookingDataFromMonday(JSON.stringify(data, null, 2));
+
+      //Flatten and reconstruct API Result and store in state
+      //Important info for client: do not change column titles
+      const flatten = (obj) => Object.values(obj).flat();
+      const flattened_data = flatten(data)[0].boards[0].items;
+      const key_properties = [
+        "Name",
+        "Kennzeichen",
+        "Datum Start",
+        "Zeit Start",
+        "Datum Ende",
+        "Zeit Ende",
+        "Bemerkung",
+        "Zusatz 1",
+        "Zusatz 2",
+        "Zusatz 3",
+        "Zusatz 4",
+        "Zusatz 5",
+        "Zusatz 6",
+        "Zusatz 7",
+        "Zusatz 8",
+        "Zusatz 9",
+        "Zusatz 10",
+      ];
+      const restructured_data = flattened_data.map((booking) => {
+        const booking_data = { id: booking.id };
+        booking.column_values.forEach((column) => {
+          if (key_properties.includes(column.title)) {
+            booking_data[column.title.toLowerCase().replace(" ", "_")] =
+              column.text;
+          }
+        });
+        return booking_data;
+      });
+      setBookingDataFromMonday(JSON.stringify(restructured_data, null, 2));
     };
+    //Invoke Fetch
     fetchBookingDataFromMonday();
   }, []);
 
   return (
-    <div className="App">
+    <div>
       <h1>Raw Booking Data</h1>
-      <p>{bookingDataFromMonday}</p>
+      <pre>{bookingDataFromMonday}</pre>
     </div>
   );
 }
